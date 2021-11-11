7fa29c70176d5601107f765f4dbbf4f686ac19a4
kimjiin <kj980926@naver.com>
2021-11-11 22:47:56 +0900

    초기 세팅

---
+++ EarlyBuddyProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarlyBuddyProj
+//
+//  Created by 김지인 on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
